47ca1d62dae27bae0eb91c7e697c4dbbc07146b4
Chien <stdnt25@gmail.com>
2017-06-28 16:24:50 +0800

    Login Form OK

--- test0628_tryC/Form1.Designer.cs
+++ test0628_tryC/Form1.Designer.cs
@@ -25,6 +25,7 @@
         private void InitializeComponent() {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -45,11 +46,21 @@
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(67, 214);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 67);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Login";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(278, 245);
+            this.ClientSize = new System.Drawing.Size(278, 321);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -62,6 +73,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
